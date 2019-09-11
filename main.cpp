a386b30839946fbee9a522729c92b2d7b3e80c2f
Albedaran <nyantaobachan@gmail.com>
2019-09-11 13:38:47 +0200

    Integers Task Complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Screaming Goat Noises" << std::endl;
+    //Practice1
+    //std::cout << "Screaming Goat Noises" << std::endl;
+    //std::cout << 42 << std::endl;
+    //std::cout << 3.1415 << std::endl;
     std::cout << 42 << std::endl;
-    std::cout << 3.1415 << std::endl;
+    std::cout << 0 << std::endl;
+    std::cout << -3 << std::endl;
+    std::cout << 2147483647 << std::endl;
+    std::cout << -2147483648 << std::endl;
+    std::cout << 2147483648 << std::endl;
     return 0;
 }
