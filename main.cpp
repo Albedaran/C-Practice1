3f7b9e9dc04f45b60efc7d50f4eab6ca6129efb1
Albedaran <nyantaobachan@gmail.com>
2019-09-11 17:14:16 +0200

    Conditions Practice completed

--- main.cpp
+++ main.cpp
@@ -253,7 +253,37 @@ int main() {
     //int sum = first+second+third+fourth+fifth;
     //std::cout << "Sum: " << sum << ", Average: " << sum/2. << std::endl;
 
+    //Practice 10
+    //int a = 13;
+    //if (a=13) {
+    //std::cout << "The value of the variable is 13. How magnificent!" << std::endl;
+    //}
 
+    //int b = 20;
+    //if (b<10) {
+    //std::cout << "The variable's value is lower than 10." << std::endl;
+    //} else {
+    //std::cout << "The variable's value is greater than 10." << std::endl;
+    //}
+
+    //int c = 15;
+    //if (c<10) {
+        //std::cout << "The variable's value is lower than 10." << std::endl;
+        //} else if (c<20) {
+        //std::cout << "The variable is lower than 20 but greater than 10." << std::endl;
+        //} else {
+        //std::cout << "The variable is greater than 20." << std::endl;
+        //}
+
+    //bool thirsty = true;
+    //bool hungry = true;
+    //if (thirsty&&hungry) {
+        //std::cout << "Lunch time baby! YEEHAAAW!" << std::endl;
+        //} else if (thirsty||hungry) {
+        //std::cout << "Snack time BOIS!" << std::endl;
+        //} else {
+        //std::cout << "Dronccc time!" << std::endl;
+        //}
 
 
 
