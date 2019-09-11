70356f45340835ba1c585caf47cc3788c6d70d8a
Albedaran <nyantaobachan@gmail.com>
2019-09-11 14:07:24 +0200

    Exercise I Task Complete

--- main.cpp
+++ main.cpp
@@ -49,11 +49,19 @@ int main() {
     //std::cout << (false||false) << std::endl;
 
     //Practice6
-    std::cout << "aeroplane" << std::endl;
-    std::cout << "don't" << std::endl;
-    std::cout << "\"Long quote goes here..\" - Aaron" <<std::endl;
+    //std::cout << "aeroplane" << std::endl;
+    //std::cout << "don't" << std::endl;
+    //std::cout << "\"Long quote goes here..\" - Aaron" <<std::endl;
 
-    std::cout << "tooth" << "brush" << std::endl;
-    std::cout << "tooth" << " " << "brush" << std::endl;
+    //std::cout << "tooth" << "brush" << std::endl;
+    //std::cout << "tooth" << " " << "brush" << std::endl;
+    //std::cout << "My lucky number is" << 7 << std::endl;
+
+    //Exercise
+    std::cout << "My name is K1W1, I'll be your AI assistant today~" << std::endl;
+    std::cout << "I have no such thing as age, I'm not a living organism. Although, if you're that curious, I was constructed " << 4 << " years ago." << std::endl;
+    std::cout << "I am 420 centimerets high" << std::endl;
+    std::cout << "HAHAAA" << std::endl;
+    std::cout << "Just kidding, silly organism. I am merely " << 40.4 << " centimeters." << std::endl;
     return 0;
 }
