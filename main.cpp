b5c0e69107c72729fb477257bfc0c2ae46a61887
Albedaran <nyantaobachan@gmail.com>
2019-09-11 13:33:22 +0200

    First Task - Printing - 1 completed

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Screaming Goat Noises" << std::endl;
+    std::cout << 42 << std::endl;
+    std::cout << 3.1415 << std::endl;
+    return 0;
+}
